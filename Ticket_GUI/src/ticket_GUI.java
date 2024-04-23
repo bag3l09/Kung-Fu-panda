905323e59456eae1a53e47d81b3fe5e41c318a37
Twintastic <110127966+Twintastic@users.noreply.github.com>
2024-04-23 15:46:10 -0400

    made it so A10 was visible

--- Ticket_GUI/src/ticket_GUI.java
+++ Ticket_GUI/src/ticket_GUI.java
@@ -81,7 +81,7 @@ public class ticket_GUI extends JFrame {
 		contentPane.add(lblThereAre);
 		
 		JLabel lblaAA = new JLabel("[A1, A2, A3, A4, A5, A6, A7, A8, A9, A10]");
-		lblaAA.setBounds(119, 111, 220, 13);
+		lblaAA.setBounds(119, 111, 230, 13);
 		contentPane.add(lblaAA);
 		
 		JLabel lblbBB = new JLabel("[B1, B2, B3, B4, B5, B6, B7, B8, B9, B10]");
